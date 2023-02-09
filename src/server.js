8893db9e1015843f94de8c6a0216c0fd9efc9956
3o14 <dldnjswn4265@gmail.com>
2023-02-09 15:36:33 +0900

    websocket 연결

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,14 @@ const app = express();
 app.set("view engine", "pug"); // viewengine을 pug로 설정
 app.set("views", __dirname + "/views"); // express로 template 위치 지정
 app.use("/public", express.static(__dirname + "/public")); // public url로 유저에게 파일 제공
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server}); 
+// wss(Web Socket Serveer)
+
+// http 서버 위에 webSocket 서버를 만듬
+server.listen(3000, handleListen);
