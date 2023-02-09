63a71152ece35e7bcc3ba9cdbf227129deb57321
3o14 <dldnjswn4265@gmail.com>
2023-02-09 15:09:39 +0900

    express, pug 설정

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hello world");
+app.set("view engine", "pug"); // viewengine을 pug로 설정
+app.set("views", __dirname + "/views"); // express로 template 위치 지정
+app.use("/public", express.static(__dirname + "/public")); // public url로 유저에게 파일 제공
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
