719cb7b6ddfdea76d1de7bbc38dff69e41295d3c
3o14 <dldnjswn4265@gmail.com>
2023-02-10 23:09:33 +0900

    emit method memo

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,13 +3,17 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 
+function backendDone(msg) {
+  console.log(`The backend says: `, msg);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {  // <-> webSocket : socket.send()
-    console.log("server is done!");
-    });
-    // emit(event, argument1, argument2, ...)
+  socket.emit("enter_room", input.value, backendDone);
+  // socketIO : socket.emit()  <->  webSocket : socket.send()
+  // emit(event, argument1, argument2, ...) 함수function도 가능
+  // emit의 parameter로 함수를 전달하기 위해서는 반드시 마지막 argument에 위치해야 한다.
   input.value = "";
 }
 
