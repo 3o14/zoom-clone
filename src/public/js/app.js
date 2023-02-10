46f536f0488c05787f9b655fc5007f31e3b8a86f
3o14 <dldnjswn4265@gmail.com>
2023-02-10 22:58:21 +0900

    emit method arguments

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {  // <-> webSocket : socket.send()
+    console.log("server is done!");
+    });
+    // emit(event, argument(object도 가능), (server에서 호출하는) function ) 
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
